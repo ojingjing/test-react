fc7ae525f5a8c579b4ace3e9afea47e22c622277
ojingjing <wlddj14@naver.com>
2024-01-08 21:25:39 +0900

    test6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          quitdfdsfsdfs
+          quitdfdsfsdfsㄷㄹㄷㄹ
         </a>
       </header>
     </div>
